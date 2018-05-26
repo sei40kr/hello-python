edf6477ca26a19959d03bbee3d7c8b697afa6d0a
Seong Yong-ju <sei40kr@gmail.com>
2018-05-27 01:06:00 +0900

    Add numbers

---
+++ 3-1-1-numbers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# classic division returns a float
+print(17.0 / 3)
+# floor division discards the fractional part
+print(17 // 3)
+# the % operator returns the remainder of the division
+print(17 % 3)
+# 2 to the power of 7
+print(2**7)
