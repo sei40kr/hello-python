a9165f1671a8cf5859ee3aa423f3d4f18bfe804c
Seong Yong-ju <sei40kr@gmail.com>
2018-05-27 01:36:23 +0900

    Add odds-and-ends

---
+++ 9-7-odds-and-ends.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+class Employee:
+    pass
+
+
+# Create an empty employee record
+john = Employee()
+
+# Fill the fields of the record
+# john.name = 'John Doe'
+# john.dept = 'computer lab'
+# john.salary = 1000
