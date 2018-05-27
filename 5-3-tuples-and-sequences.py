e48f387ba5fcd3c7ad3618a3e6ae36c35b7a4129
Seong Yong-ju <sei40kr@gmail.com>
2018-05-28 00:31:59 +0900

    Add tuples-and-sequences

---
+++ 5-3-tuples-and-sequences.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+t = 12345, 54321, 'hello!'
+print(t[0])
+print(t)
+
+# Tuples may be nested:
+u = t, (1, 2, 3, 4, 5)
+print(u)
+
+# error: Tuples are immutable:
+# t[0] = 88888
+
+# but they can contain mutable objects:
+v = ([1, 2, 3], [3, 2, 1])
+print(v)
